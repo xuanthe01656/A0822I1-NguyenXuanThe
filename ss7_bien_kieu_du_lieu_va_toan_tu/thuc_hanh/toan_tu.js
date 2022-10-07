a92753082bd0c658371ac54fa0340ad8c70d8248
xuanthe01656 <xuanthe01656@gmail.com>
2022-10-07 17:00:51 +0700

    up

---
+++ ss7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/toan_tu.js
@@ -0,0 +1,8 @@
+function dientich(){
+let inputWidth;
+inputWidth = parseInt(prompt("Enter the width")) ;
+let inputHeight;
+inputHeight = + prompt("Enter the height");
+let area = inputWidth * inputHeight;
+    document.getElementById("id").innerHTML="The area is: " + area;
+}
